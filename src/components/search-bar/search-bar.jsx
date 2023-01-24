d32a1df808a1bf7570323c39eb8fa480b40dde44
muhammad1749 <muhammadqodirmuminov2@gmail>
2023-01-24 16:54:41 +0500

    salom

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,8 +1,21 @@
 import { Search } from "@mui/icons-material";
 import { Paper, IconButton } from "@mui/material";
 import { colors } from "../../constants/colors";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+	const [value, setValue] = useState("");
+	const navigate = useNavigate();
+
+	const onSearchHandler = (e) => {
+		e.preventDefault();
+
+		console.log(value);
+		if (value) {
+			navigate(`/search/${value}`);
+		}
+	};
 	return (
 		<Paper
 			sx={{
@@ -10,9 +23,16 @@ const SearchBar = () => {
 				paddingLeft: 2,
 				boxShadow: "none",
 			}}
-			component={"form"}>
-			<input type="text" placeholder="Search..." className="search-bar" />
-			<IconButton>
+			component={"form"}
+			onSubmit={onSearchHandler}>
+			<input
+				type="text"
+				placeholder="Search..."
+				className="search-bar"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+			<IconButton type="submit">
 				<Search />
 			</IconButton>
 		</Paper>
--- src/service/apiServise.js
+++ src/service/apiServise.js
@@ -3,18 +3,22 @@ import axios from "axios";
 const BASE_URL = "https://youtube-v3-alternative.p.rapidapi.com";
 
 const options = {
+	method: "GET",
+	url: "https://youtube-v31.p.rapidapi.com/search",
 	params: {
-		maxResults: 50,
+		
+		maxResults: "50",
+	
 	},
 	headers: {
 		"X-RapidAPI-Key": "2b09ee226cmsh3efc88d0141ae38p15130ejsnf1c0f85b04fc",
-		"X-RapidAPI-Host": "youtube-v3-alternative.p.rapidapi.com",
+		"X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
 	},
 };
 
 export const ApiService = {
 	async fatching(url) {
 		const responce = await axios.get(`${BASE_URL}/${url}`, options);
-		return responce;
+		return responce.data;
 	},
 };
